73a47c0216e34adc8a889dd2741b1d119ffdf5e5
Nandakumar003 <nandakumar.s1999@gmail.com>
2024-05-09 14:41:04 -0400

    wip

--- public/Scripts/reset.js
+++ public/Scripts/reset.js
@@ -3,6 +3,8 @@ document.getElementById("reset-credentials").addEventListener('submit', ResetCre
 function ResetCredentials(e) {
     e.preventDefault();
     var formData = {
-        Username: document.getElementById("usernameoremail").value,
+        Username: document.getElementById("usernameoremail").value
+
     };
     console.log(formData);
+}

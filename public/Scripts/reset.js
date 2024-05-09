f185b9c19f448bbaed5b1ca5d388a409d2f7ade7
Nandakumar003 <nandakumar.s1999@gmail.com>
2024-05-09 14:59:38 -0400

    WIP

--- public/Scripts/reset.js
+++ public/Scripts/reset.js
@@ -2,9 +2,30 @@
 document.getElementById("reset-credentials").addEventListener('submit', ResetCredentials);
 function ResetCredentials(e) {
     e.preventDefault();
-    var formData = {
-        Username: document.getElementById("usernameoremail").value
-
-    };
-    console.log(formData);
+    var queryParams = {
+        username: document.getElementById("usernameoremail").value,
+        useremail: document.getElementById("usernameoremail").value,
+        userid: document.getElementById("usernameoremail").value
+    }
+    const url = '/getUserData';
+    const queryString = new URLSearchParams(queryParams).toString();
+    const fullUrl = `http://localhost:3000/users${url}?${queryString}`;
+    fetch(fullUrl)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(`Username/Email doesn't exist`);
+            }
+            return response.json();
+        })
+        .then(data => {
+            // Handle the response data
+            console.log(data);
+        })
+        .catch(error => {
+            // Handle errors
+            console.error(error);
+            let err = document.getElementById("error-message")
+            err.style.color = "red";
+            err.innerHTML = `${error.message}`
+        });
 }

9e9d207b9cfb1bb9966cd3d221df4a7a4b02b00b
Nandakumar003 <nandakumar.s1999@gmail.com>
2024-05-13 16:31:40 -0400

    WIP

--- public/Scripts/delete.js
+++ public/Scripts/delete.js
@@ -1,12 +1,8 @@
 function confirmDelete() {
-    // Display confirmation dialog
     const result = confirm('Are you sure you want to delete your account?');
-
-    // Check user's choice
     if (result) {
-        // User clicked OK
+        localStorage.removeItem('user')
         alert('Account deleted successfully.');
-        // Redirect to home page
         window.location.href = 'index.html';
     } else {
         // User clicked Cancel or closed the dialog
--- public/Scripts/main.js
+++ public/Scripts/main.js
@@ -50,15 +50,14 @@ if (p1) {
 }
 
 function confirmDelete() {
-  // Display confirmation dialog
   const result = confirm('Are you sure you want to delete your account?');
-
   // Check user's choice
   if (result) {
     // User clicked OK
     alert('Account deleted successfully.');
-    // Redirect to home page
-    window.location.href = 'index.html';
+    localStorage.removeItem('tempuser')
+    removeUser()
+
   } else {
     // User clicked Cancel or closed the dialog
     // Do nothing or any other action you want
